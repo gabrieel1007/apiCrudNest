63d0ef628d46a88ff176c44d6bd132169449b1d6
Gabriel Alves <ga654066@gmail.com>
2024-06-15 11:17:33 -0300

    api nest crud

---
+++ src/developers/entities/developer.entity.ts
@@ -0,0 +1,27 @@
+/* eslint-disable prettier/prettier */
+import { BeforeInsert, Column, Entity, PrimaryColumn } from "typeorm";
+
+//eslint-disable-next-line @typescript-eslint/no-var-requires
+const {nanoid} = require("nanoid");
+
+
+@Entity('developers')
+export class Developer {
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    dateOfBirth: string;
+
+    @BeforeInsert()
+    generateId(){
+        this.id = `dev_${nanoid()}`;
+    }
+    
+}
